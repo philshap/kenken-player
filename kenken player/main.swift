0d588abfb73b9bf9d4544904ef62238d038e308d
Phil Shapiro <phil@Phil-Shapiros-Computer-3.local>
2014-08-16 09:36:06 -0400

    Initial Commit

---
+++ kenken player/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  kenken player
+//
+//  Created by Phil Shapiro on 8/16/14.
+//  Copyright (c) 2014 Phil Shapiro. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
