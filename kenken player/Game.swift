f63c51ba50b53b3d0b92c624099c28edf2a38b2a
Phil Shapiro <phil@Phil-Shapiros-Computer-3.local>
2014-08-17 17:09:29 -0400

    Move test games into the test code

--- kenken player/Game.swift
+++ kenken player/Game.swift
@@ -21,33 +21,6 @@ public struct Game {
         self.constraints = constraints;
     }
     
-    // Trivial 2x2 game where each cell is a fixed value.
-    public static func createTestGame() -> Game {
-        return Game(size: 2, constraints: [
-            Constraint(Operator.Identity, 1, [ Position(column: 0, row: 0) ]),
-            Constraint(Operator.Identity, 2, [ Position(column: 0, row: 1) ]),
-            Constraint(Operator.Identity, 2, [ Position(column: 1, row: 0) ]),
-            Constraint(Operator.Identity, 1, [ Position(column: 1, row: 1) ]),
-        ]);
-    }
-    
-    // KenKen puzzle from NYTimes 08/17/14
-    public static func createTestGame2() -> Game {
-        return Game(size: 5, constraints: [
-            Constraint(Operator.Plus, 3, [ Position(column: 0, row: 0), Position(column: 0, row: 1) ]),
-            Constraint(Operator.Identity, 2, [ Position(column: 1, row: 0) ]),
-            Constraint(Operator.Minus, 2, [ Position(column: 2, row: 0), Position(column: 2, row: 1) ]),
-            Constraint(Operator.Minus, 1, [ Position(column: 3, row: 0), Position(column: 4, row: 0) ]),
-            Constraint(Operator.Minus, 1, [ Position(column: 1, row: 1), Position(column: 1, row: 2) ]),
-            Constraint(Operator.Multiply, 50, [ Position(column: 3, row: 1), Position(column: 4, row: 1), Position(column: 2, row: 2) , Position(column: 3, row: 2) ]),
-            Constraint(Operator.Plus, 9, [ Position(column: 0, row: 2), Position(column: 0, row: 3) ]),
-            Constraint(Operator.Minus, 2, [ Position(column: 4, row: 2), Position(column: 4, row: 3) ]),
-            Constraint(Operator.Plus, 9, [ Position(column: 1, row: 3), Position(column: 0, row: 4), Position(column: 1, row: 4) ]),
-            Constraint(Operator.Minus, 3, [ Position(column: 2, row: 3), Position(column: 2, row: 4) ]),
-            Constraint(Operator.Identity, 2, [ Position(column: 4, row: 4) ]),
-            ]);
-    }
-    
     public func applyConstraints() {
         for constraint in constraints {
             constraint.constrain(board);
--- kenken playerTests/GameTest.swift
+++ kenken playerTests/GameTest.swift
@@ -22,15 +22,42 @@ class GameTest: XCTestCase {
         super.tearDown()
     }
     
+    // Trivial 2x2 game where each cell is a fixed value.
+    func createTestGame() -> Game {
+        return Game(size: 2, constraints: [
+            Constraint(Operator.Identity, 1, [ Position(column: 0, row: 0) ]),
+            Constraint(Operator.Identity, 2, [ Position(column: 0, row: 1) ]),
+            Constraint(Operator.Identity, 2, [ Position(column: 1, row: 0) ]),
+            Constraint(Operator.Identity, 1, [ Position(column: 1, row: 1) ]),
+            ]);
+    }
+    
+    // KenKen puzzle from NYTimes 08/17/14
+    func createTestGame2() -> Game {
+        return Game(size: 5, constraints: [
+            Constraint(Operator.Plus, 3, [ Position(column: 0, row: 0), Position(column: 0, row: 1) ]),
+            Constraint(Operator.Identity, 2, [ Position(column: 1, row: 0) ]),
+            Constraint(Operator.Minus, 2, [ Position(column: 2, row: 0), Position(column: 2, row: 1) ]),
+            Constraint(Operator.Minus, 1, [ Position(column: 3, row: 0), Position(column: 4, row: 0) ]),
+            Constraint(Operator.Minus, 1, [ Position(column: 1, row: 1), Position(column: 1, row: 2) ]),
+            Constraint(Operator.Multiply, 50, [ Position(column: 3, row: 1), Position(column: 4, row: 1), Position(column: 2, row: 2) , Position(column: 3, row: 2) ]),
+            Constraint(Operator.Plus, 9, [ Position(column: 0, row: 2), Position(column: 0, row: 3) ]),
+            Constraint(Operator.Minus, 2, [ Position(column: 4, row: 2), Position(column: 4, row: 3) ]),
+            Constraint(Operator.Plus, 9, [ Position(column: 1, row: 3), Position(column: 0, row: 4), Position(column: 1, row: 4) ]),
+            Constraint(Operator.Minus, 3, [ Position(column: 2, row: 3), Position(column: 2, row: 4) ]),
+            Constraint(Operator.Identity, 2, [ Position(column: 4, row: 4) ]),
+            ]);
+    }
+    
     func testGame() {
-        var game = Game.createTestGame();
+        var game = createTestGame();
         XCTAssert(!game.isGameComplete());
         game.applyConstraints();
         XCTAssert(game.isGameComplete());
     }
     
     func testGameBig() {
-        var game = Game.createTestGame2();
+        var game = createTestGame2();
         XCTAssert(!game.isGameComplete());
         for i in 0...10 {
             game.applyConstraints();
